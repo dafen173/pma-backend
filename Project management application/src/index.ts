572039bca519b39758c01376ec57a968663f0127
dafen173 <dafen@ukr.net>
2023-04-14 15:04:12 +0300

    fix: add mongodb data

--- Project management application/src/index.ts
+++ Project management application/src/index.ts
@@ -6,7 +6,8 @@ import * as serverService from './services/server.service';
 
 (async () => {
   try {
-    await mongoose.connect('mongodb+srv://${UserName}:${Password}@${clasterInfo}.mongodb.net/managerApp');
+    // await mongoose.connect('mongodb+srv://${UserName}:${Password}@${clasterInfo}.mongodb.net/managerApp');
+    await mongoose.connect('mongodb+srv://dafen:fTGzIB8fAub2NAoJ@cluster0-pma.uybgwce.mongodb.net/managerApp');
     serverService.server.listen(process.env.PORT || PORT, function () {
       console.log('Сервер ожидает подключения...');
     })
